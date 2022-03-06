e7819f8daa69385226e31d735cd781105b1d11fd
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-06 19:33:32 -0300

    feat(button): create story

---
+++ src/components/button/button.stories.js
@@ -0,0 +1,27 @@
+import { html } from 'lit';
+import '.';
+
+export default {
+  title: 'Components/Button'
+}
+
+export const Button = ({
+  label,
+  disabled,
+  loading
+}) => {
+  return html`
+    <dsc-button
+      ?disabled="${disabled}"
+      ?loading="${loading}"
+    >
+      ${label}
+    </dsc-button>
+  `
+}
+
+Button.args = {
+  label: "Button Primary",
+  disabled: false,
+  loading: false
+}

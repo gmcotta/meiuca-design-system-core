eb7466510ef8794243347e38e775c30bcda2c1c7
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-21 10:19:44 -0300

    feat(radio): create radio-group component, styles and stories

--- src/components/radio/index.js
+++ src/components/radio/index.js
@@ -1,13 +1,14 @@
 import { LitElement, html, unsafeCSS } from 'lit';
 import { classMap } from 'lit/directives/class-map.js';
 
-import bullet from '@meiuca/dsc-assets/dist/assets/icons/bullet';
+import bullet from '@gmcotta/meiuca-assets/dist/assets/icons/bullet';
 import '../icon'
 
 import style from './radio.styles.scss';
 
 export default class DscRadio extends LitElement {
-  static get styles() {
+
+  static get styles(){
     return unsafeCSS(style);
   }
 
@@ -36,35 +37,69 @@ export default class DscRadio extends LitElement {
     this.disabled = false;
   }
 
+  get hiddenInput() {
+    return this.shadowRoot?.querySelector('input');
+  }
+
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+        if (propName === 'checked' && oldValue !== undefined) {
+          console.log(this.checked);
+          this.hiddenInput.checked = this.checked;
+        }
+    });
+  }
+
+  _handleClick() {
+    this.checked = true;
+    this._handleDispatchEvent();
+  }
+
+  _handleDispatchEvent = () => {
+    this.dispatchEvent(new CustomEvent('dscChange', {
+      detail: {
+          checked: this.checked,
+          index: this.index,
+          value: this.value
+      },
+      bubbles: true,
+      composed: true
+    }));
+  }
+
   render() {
     return html`
       <label
         class="${classMap({
-          ['radio']: true,
-          ['radio--checked']: this.checked,
-          ['radio--disabled']: this.disabled,
+          [`radio`]: true,
+          [`radio--checked`]: this.checked,
+          [`radio--disabled`]: this.disabled,
         })}"
         for="${this.name}"
       >
-        <input 
+        <input
           type="radio"
           .id="${this.id}"
           .name="${this.name}"
           .value="${this.value}"
-          ?disabled="${this.disabled}"
-          ?checked="${this.checked}"
-          ?required="${this.required}"
-          aria-checked="${this.checked}"
+          ?disabled=${this.disabled}
+          ?checked=${this.checked}
+          ?required=${this.required}
+          aria-checked=${this.checked}
           @click="${this._handleClick}"
-        />
+        >
         <span class="radio-checkmark">
-          ${this.checked 
-            ? html`<dsc-icon .icon="${bullet}" size="sm"></dsc-icon>`
+          ${ this.checked ?
+              html`<dsc-icon .icon="${bullet}" size="sm"></dsc-icon>`
             : null
           }
         </span>
         ${this.label}
       </label>
-    `;
+    `
   }
+}
+
+if (!customElements.get('dsc-radio')) {
+  customElements.define('dscr-radio', DscRadio);
 }
